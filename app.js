ad8c7c4bad24dcc563375acb9d3a72b1d50afd30
lsh-anyway <1138674510@qq.com>
2017-10-06 13:07:51 +0800

    finished with test

--- app.js
+++ app.js
@@ -15,6 +15,27 @@ const multipart = require('connect-multiparty')
 mongoose.Promise = global.Promise
 mongoose.connect(dbUrl,{useMongoClient: true})
 
+// models loading
+let models_path = __dirname + '/app/models'
+let walk = function(path) {
+  fs
+    .readdirSync(path)
+    .forEach(function(file) {
+      let newPath = path + '/' + file
+      let stat = fs.statSync(newPath)
+
+      if (stat.isFile()) {
+        if (/(.*)\.(js|coffee)/.test(file)) {
+          require(newPath)
+        }
+      }
+      else if (stat.isDirectory()) {
+        walk(newPath)
+      }
+    })
+}
+walk(models_path)
+
 app.set('views', './app/views/pages')
 app.set('view engine', 'jade')
 app.use(bodyParser.urlencoded({extended: true}))
